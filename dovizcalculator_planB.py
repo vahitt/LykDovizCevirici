666402407160cb995ade99d92e150a66dc6c3162
Betül Arı <betulari19996@gmail.com>
2018-08-02 17:49:29 +0300

    getRequest fixed

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -40,10 +40,23 @@ class Doviz():
         pass
     
     def toplu_cevir(self):
-        pass
+        birinci_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
+        ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
+        miktar = float(input("Ne kadar bu birimden paranız var:"))
+
+        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(
+                doviz=birinci_doviz, doviz2=ikinci_doviz)
+
+        self.getRequest(url)
+        print("{tarihli} tarihi itibariyle {ilkpara} {ilkbirim} ----->{ikinicipara} {ikinicibirim} değerindedir.".format(tarihli=self.tarih,ilkpara=float(miktar),ilkbirim=birinci_doviz,ikinicipara= float(self.deger) * miktar,ikinicibirim=ikinci_doviz))
     
     def getRequest(self,url):
         self.url = url
         raw_data = self.requests_session.get(self.url).content
         dovizData = json.loads(raw_data)
         #TARIH VE DEGER CEKILECEK
+        degerler = dovizData.get('Realtime Currency Exchange Rate')
+        self.tarih=degerler.get('6. Last Refreshed')
+        self.deger=degerler.get('5. Exchange Rate')
+
+doviz=Doviz()
