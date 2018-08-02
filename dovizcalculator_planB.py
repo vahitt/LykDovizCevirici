0fec9156c465714f0ce723a7656c4f47a187371c
hayriyigit <hayriyigit@outlook.com.tr>
2018-08-02 17:14:47 +0300

    Added some magic too

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -25,7 +25,7 @@ class Doviz():
         elif secim == 2:
             return self.cevir()
         elif secim == 3:
-            return self.cevir()
+            return self.toplu_cevir()
         else:
             print("Lutfen gecerli bir secim yapiniz")
             return self.__init__()
@@ -34,10 +34,12 @@ class Doviz():
         dolar = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='usd', doviz2='try')
         sterlin = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='gbp', doviz2='try')
         euro ="https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='eur', doviz2='try')
-        
         pass
     
     def cevir(self):
+        pass
+    
+    def toplu_cevir(self):
         pass
     
     def getRequest(self,url):
@@ -45,10 +47,3 @@ class Doviz():
         raw_data = self.requests_session.get(self.url).content
         dovizData = json.loads(raw_data)
         #TARIH VE DEGER CEKILECEK
-
-
-istenen_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
-ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
-
-doviz = Doviz("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz=istenen_doviz, doviz2=ikinci_doviz),requests.session())
-doviz.getRequest()
