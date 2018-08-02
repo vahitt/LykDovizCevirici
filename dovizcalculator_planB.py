ce7d4952acb8b36f2fd5ebbafe97c148fbc2ce3f
Ilter Kose <ilterkose98@icloud.com>
2018-08-02 15:15:08 +0300

    doviz urllib

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -1,36 +1,21 @@
-"""
-import json
-import requests
-
-istenen_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
-
-data = requests.get('https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency=TRY&apikey=5Y57NAHCNN1GRX1O'.format(doviz = istenen_doviz))
-
-codeco = data.content.decode ("UTF-8")
-
-suggests_json = codeco.split("Realtime Currency Exchange Rate")[0:]
-
-suggests = json.loads(codeco)
-
-for i in suggests:
-    print(i)
-"""
-
-
 import json
 from urllib.request import urlopen
 
 istenen_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
+ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
 
-with urlopen("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency=TRY&apikey=5Y57NAHCNN1GRX1O".format(doviz = istenen_doviz)) as sterlingRequest:
-    sterlingSource = sterlingRequest.read()
+with urlopen("https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz = istenen_doviz,doviz2 = ikinci_doviz)) as sterlingRequest:
+    dovizSource = sterlingRequest.read()
 
-sterlingData = json.loads(sterlingSource)
+dovizData = json.loads(dovizSource)
 
-def getSterlingName():
-    sterlingName = sterlingData["Realtime Currency Exchange Rate"]["2. From_Currency Name"]
-    print(sterlingSource)
-    return sterlingName
+def getdovizName():
+    dovizName = dovizData["Realtime Currency Exchange Rate"]["2. From_Currency Name"]
+    print(dovizSource)
+    return dovizName
 
+def dovizCalculate():
 
-getSterlingName()
+    result = dovizName.get(2) - dovizName.get(3)
+    return result
+
