7e8307fb8b4253c1b4cfa4ae209d5e6d50c9825c
Vahit Kuruosman <kuruosmanvahit@gmail.com>
2018-08-03 16:00:08 +0300

    tarih api eklendi.Url kontrolu yapıldı

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -10,7 +10,7 @@ class Doviz():
         1 - TL -> USD EURO STERLIN GOSTER
         2 - DEGER CEVIR
         3 - TOPLU CEVIR 
-        
+        4 - HAFTALIK PARA BİRİMİNİN KARSILIGINI GOSTER
         seciminiz:             
         """)
 
@@ -26,6 +26,10 @@ class Doviz():
             return self.cevir()
         elif secim == 3:
             return self.toplu_cevir()
+        elif secim == 4:
+            hangi_dovizden = input("Hangi dovizin karsılığını görmek istiyorusunuz:")
+            hangi_dovize = input("Karsılığını gostermek istediğiniz dövizi giriniz:")
+            return  self.showWeek(hangi_dovizden,hangi_dovize)
         else:
             pprint("Lutfen gecerli bir secim yapiniz")
             return self.__init__()
@@ -52,14 +56,17 @@ class Doviz():
 
     def cevir(self):
         pass
-    
+    def showWeek(self,h_dovizden,h_dovize):
+        url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol={hangi_dovizden}&to_symbol" \
+              "={hangi_dovize}&apikey=5Y57NAHCNN1GRX1O".format(hangi_dovizden = h_dovizden,hangi_dovize=h_dovize)
+        self.getRequest(url)
     def toplu_cevir(self):
         birinci_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
         ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
         miktar = float(input("Ne kadar bu birimden paranız var:"))
 
-        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(
-                doviz=birinci_doviz, doviz2=ikinci_doviz)
+        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency=" \
+              "{doviz2}&apikey=5Y57NAHCNN1GRX1O".format( doviz=birinci_doviz, doviz2=ikinci_doviz)
 
         self.getRequest(url)
         print("{tarihli} tarihi itibariyle {ilkpara} {ilkbirim} ----->{ikinicipara} {ikinicibirim} değerindedir.".format(tarihli=self.tarih,ilkpara=float(miktar),ilkbirim=birinci_doviz,ikinicipara= float(self.deger) * miktar,ikinicibirim=ikinci_doviz))
@@ -68,10 +75,15 @@ class Doviz():
         self.url = url
         raw_data = self.requests_session.get(self.url).content
         dovizData = json.loads(raw_data)
-        #TARIH VE DEGER CEKILECEK
-        degerler = dovizData.get('Realtime Currency Exchange Rate')
-        self.tarih=degerler.get('6. Last Refreshed')
-        self.deger=degerler.get('5. Exchange Rate')
+
+        if self.url.split("=")[1].split("&")[0] == "FX_WEEKLY":
+            print(dovizData)
+        else:
+            #TARIH VE DEGER CEKILECEK
+            degerler = dovizData.get('Realtime Currency Exchange Rate')
+            self.tarih=degerler.get('6. Last Refreshed')
+            self.deger=degerler.get('5. Exchange Rate')
+
 
 doviz=Doviz()
 
