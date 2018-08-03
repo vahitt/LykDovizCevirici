823364c8b06d7a5a0d9da14a4494069e59ee8ab1
hayriyigit <hayriyigit@outlook.com.tr>
2018-08-03 16:59:36 +0300

    API Fixed and some magic added

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -3,9 +3,13 @@ import requests
 from pprint import pprint
 
 class Doviz():
-    requests_session = requests.session()
-
+    
+    
+    
     def __init__(self):
+        self.api_url = "https://exchangeratesapi.io/api/latest?base={}"
+        self.requests_session = requests.session()
+        
         secim = input("""
         1 - TL -> USD EURO STERLIN GOSTER
         2 - DEGER CEVIR
@@ -35,27 +39,26 @@ class Doviz():
             return self.__init__()
     
     def tr_deger(self):
-        dolar = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='usd', doviz2='try')
-        sterlin = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='gbp', doviz2='try')
-        euro ="https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency={doviz2}&apikey=5Y57NAHCNN1GRX1O".format(doviz='eur', doviz2='try')
-        print("1TL karsılıkları\n")
-
-        for i in range(3):
-            if(i == 0):
-                self.getRequest(dolar)
-                dolar_data = self.deger
-                print("USD->{usd}\n" .format(usd =dolar_data) )
-            elif(i==1):
-                 self.getRequest(sterlin)
-                 sterlin_data = self.deger
-                 print("GBP->{gbp}\n".format(gbp = sterlin_data))
-            elif(i == 2):
-                 self.getRequest(euro)
-                 euro_data = self.deger
-                 print("EURO->{euro}\n".format(euro=euro_data))
+        base = "TRY"
+        dolar = self.api_url.format("USD")
+        euro = self.api_url.format("EUR")
+        sterlin = self.api_url.format("GBP")
+        
+       
+        self.getRequest(dolar,base)
+        dolar_data = self.deger
+        self.getRequest(sterlin,base)
+        sterlin_data = self.deger
+        self.getRequest(euro,base)
+        euro_data = self.deger
+        
+        print("\nDolar : {}\nEuro : {}\nSterlin : {}".format(dolar_data,euro_data,sterlin_data))
+        
+        return self.__init__()
 
     def cevir(self):
         pass
+    
     def showWeek(self,h_dovizden,h_dovize):
         url = "https://www.alphavantage.co/query?function=FX_WEEKLY&from_symbol={hangi_dovizden}&to_symbol" \
               "={hangi_dovize}&apikey=5Y57NAHCNN1GRX1O".format(hangi_dovizden = h_dovizden,hangi_dovize=h_dovize)
@@ -64,25 +67,23 @@ class Doviz():
         birinci_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
         ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
         miktar = float(input("Ne kadar bu birimden paranız var:"))
-
-        url = "https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency={doviz}&to_currency=" \
-              "{doviz2}&apikey=5Y57NAHCNN1GRX1O".format( doviz=birinci_doviz, doviz2=ikinci_doviz)
-
-        self.getRequest(url)
+        
+        url = self.api_url.format(birinci_doviz.upper())
+        
+        self.getRequest(url,ikinci_doviz.upper())
         print("{tarihli} tarihi itibariyle {ilkpara} {ilkbirim} ----->{ikinicipara} {ikinicibirim} değerindedir.".format(tarihli=self.tarih,ilkpara=float(miktar),ilkbirim=birinci_doviz,ikinicipara= float(self.deger) * miktar,ikinicibirim=ikinci_doviz))
     
-    def getRequest(self,url):
+    def getRequest(self,url,base=None):
         self.url = url
+        self.base = base
         raw_data = self.requests_session.get(self.url).content
         dovizData = json.loads(raw_data)
 
         if self.url.split("=")[1].split("&")[0] == "FX_WEEKLY":
             print(dovizData)
         else:
-            #TARIH VE DEGER CEKILECEK
-            degerler = dovizData.get('Realtime Currency Exchange Rate')
-            self.tarih=degerler.get('6. Last Refreshed')
-            self.deger=degerler.get('5. Exchange Rate')
+            self.deger=dovizData.get("rates").get(self.base)
+            self.tarih=dovizData.get('date')
 
 
 doviz=Doviz()
