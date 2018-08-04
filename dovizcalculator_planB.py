13490101ff1637a5ac871b28897102cff7e8b31a
hayriyigit <hayriyigit@outlook.com.tr>
2018-08-04 10:44:01 +0300

    logging eklendi try-exec duzeltildi

--- dovizcalculator_planB.py
+++ dovizcalculator_planB.py
@@ -1,15 +1,17 @@
 import json
 import requests
+import logging
 
 class Doviz():
-    
-    
     
     def __init__(self):
         self.api_url = "https://exchangeratesapi.io/api/latest?base={}"
         self.monthly_url ="https://www.alphavantage.co/query?function=FX_MONTHLY&from_symbol={}&" \
              "to_symbol={}&apikey=5Y57NAHCNN1GRX1O"
         self.requests_session = requests.session()
+        
+        format_ = "[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s"
+        logging.basicConfig(filename="warnings.log", format=format_, level=logging.INFO)
         
         secim = input("""
         1 - TL -> USD EURO STERLIN GOSTER
@@ -32,6 +34,7 @@ class Doviz():
             return  self.showMonthly()
         else:
             print("Lutfen gecerli bir secim yapiniz")
+            logging.warning("Hatali menu girdisi")
             return self.__init__()
     
     def trDeger(self):
@@ -53,23 +56,28 @@ class Doviz():
     
     def showMonthly(self):
         hangi_dovizden = input("Degisim hangi para birimine gore baz alinacak:")
-        hangi_dovize = input("Karsılığını gostermek istediğiniz birimi giriniz:")
+        hangi_dovize = input("Karsiligini gostermek istediginiz birimi giriniz:")
         url = self.monthly_url.format(hangi_dovizden,hangi_dovize)
         
         try:
             self.getRequest(url)
         except:
             print("Hatali birim girisi ya da zaman asimi")
+            logging.warning("Hatali birim girisi ya da zaman asimi")
             return self.__init__()
         
-        for keys in self.monthly_data.keys():
-            print("Tarih: {}   Kapanis: {}".format(keys,self.monthly_data.get(keys).get('4. close')))
+        try:
+            for keys in self.monthly_data.keys():
+                print("Tarih: {}   Kapanis: {}".format(keys,self.monthly_data.get(keys).get('4. close')))
+        except:
+            print("Aylik data cekilirken hata")
+            logging.warning("Aylik data cekilirken hata")
         
         return self.__init__()
     
     def topluCevir(self):
-        birinci_doviz = input("Hangi döviz türünü girmek istiyorsunuz:")
-        ikinci_doviz = input("Hangi parabirimine göre görmek istiyorsunuz:")
+        birinci_doviz = input("Hangi doviz turunu girmek istiyorsunuz:")
+        ikinci_doviz = input("Hangi parabirimine gore gormek istiyorsunuz:")
         miktar = float(input("Ne kadar bu birimden paranız var:"))
         
         url = self.api_url.format(birinci_doviz.upper())
@@ -78,23 +86,26 @@ class Doviz():
             self.getRequest(url,ikinci_doviz.upper())
         except:
             print("Hatali birim girisi ya da zaman asimi")
+            logging.warning("Hatali birim girisi ya da zaman asimi")
             return self.__init__()
         
-        print("\n{tarihli} tarihi itibariyle\n{ilkpara} {ilkbirim} {ikinicipara} {ikinicibirim} değerindedir.".format(tarihli=self.tarih,ilkpara=float(miktar),ilkbirim=birinci_doviz,ikinicipara= float(self.deger) * miktar,ikinicibirim=ikinci_doviz))
+        print("\n{tarihli} tarihi itibariyle\n{ilkpara} {ilkbirim} {ikinicipara} {ikinicibirim} degerindedir.".format(tarihli=self.tarih,ilkpara=float(miktar),ilkbirim=birinci_doviz,ikinicipara= float(self.deger) * miktar,ikinicibirim=ikinci_doviz))
         
         return self.__init__()
     
     def getRequest(self,url,base=None):
         self.url = url
         self.base = base
+        
         raw_data = self.requests_session.get(self.url).content
         dovizData = json.loads(raw_data)
 
         if self.url.split("=")[1].split("&")[0] == "FX_MONTHLY":
             self.monthly_data = dovizData.get("Time Series FX (Monthly)")
+            logging.info("Aylik data istendi")
         else:
             self.deger=dovizData.get("rates").get(self.base)
             self.tarih=dovizData.get('date')
-
+            logging.info("Ceviri datasi istendi")
 
 doviz=Doviz()
